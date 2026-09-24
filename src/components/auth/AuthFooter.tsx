71fdb3762df6f214ba3898990733b557b9610966
kollo <usekollohq@gmail.com>
2026-09-24 10:56:01 +0100

    final commit

--- src/components/auth/AuthFooter.tsx
+++ src/components/auth/AuthFooter.tsx
@@ -1,21 +1,21 @@
 export default function AuthFooter() {
-	return (
-		// footer
-		<footer className="hidden border-t border-white/20 md:block">
-			<div className="custom-container flex items-center justify-between py-6 text-sm text-white/90">
-				<span>© {new Date().getFullYear()} – Usekollo</span>
+  return (
+    // footer
+    <footer className="hidden border-t border-white/20 md:block">
+      <div className="custom-container flex items-center justify-between py-6 text-sm text-white/90">
+        <span>© {new Date().getFullYear()} – Usekollo</span>
 
-				<div className="flex items-center gap-8">
-					<a href="#" className="hover:opacity-80">
-						Cookies
-					</a>
-					<a href="#" className="hover:opacity-80">
-						Privacy
-					</a>
-				</div>
-
-				<span>English US 🇺🇸</span>
-			</div>
-		</footer>
-	);
+        <div className="flex items-center gap-8">
+          <a href="#" className="hover:opacity-80">
+            Cookies
+          </a>
+          <a href="#" className="hover:opacity-80">
+            Privacy
+          </a>
+        </div>
+        {/* usa */}
+        <span>English US 🇺🇸</span>
+      </div>
+    </footer>
+  );
 }
